952f4f75bd59496ed02e6ec7595203a5544cf963
SawshaDev <contactcallimarie@gmail.com>
2022-05-06 06:22:30 +0000

    Initial Commit

---
+++ server.py
@@ -0,0 +1,22 @@
+import pathlib
+import secrets
+import aiohttp
+import aiofiles
+from quart import Quart, jsonify, send_from_directory, url_for, request, render_template
+import random
+import os
+
+app = Quart(__name__)
+
+
+@app.route('/<filename>')
+async def sendfile(filename=None):
+    dir = "/root/yanpdb/nsfw_cdn/"
+    p = pathlib.Path(dir)
+
+    for f in p.rglob(filename):
+        print(str(f.parent))
+    
+    return await send_from_directory(str(f.parent),filename)
+
+app.run(debug=True, port=2031)
