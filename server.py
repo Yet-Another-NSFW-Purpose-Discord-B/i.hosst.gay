dc0c8d99943a01854145399e7cbfd149aa2556a8
SawshaDev <contactcallimarie@gmail.com>
2022-05-07 09:01:38 +0000

    stfff

--- server.py
+++ server.py
@@ -23,15 +23,18 @@ async def home():
 async def search(filename):
     dir = "/root/yanpdb/nsfw_cdn/"
     p = pathlib.Path(dir)
-        
+
+
     for f in p.rglob(filename):
         print(str(f.parent))
 
+    
+
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/hentai"):
-        return jsonify(url="https://thino.pics/api/v1/hentai", image=f"https://i.thino.pics/{filename}")
+        return jsonify(url="https://thino.pics/api/v1/hentai", image=f"https://i.thino.pics/{filename}", dir=f"{str(f.resolve)}")
 
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/helltakerpics"):
-        return jsonify(url="https://thino.pics/api/v1/helltaker", image=f"https://i.thino.pics/{filename}")
+        return jsonify(url="https://thino.pics/api/v1/helltaker", image=f"https://i.thino.pics/{filename}",dir=f"{str(f.resolve)}")
 
 
 
@@ -41,9 +44,12 @@ async def sendfile(filename=None):
     dir = "/root/yanpdb/nsfw_cdn/"
     p = pathlib.Path(dir)
 
+
     for f in p.rglob(filename):
         print(str(f.parent))
     
-    return await send_from_directory(str(f.parent),filename)
+
+    return await send_from_directory(str(f.parent),filename)    
+
 
 app.run(debug=True, port=2031)
