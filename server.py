fe0951187e57bd4b73b363397600b08f16bde07d
SawshaDev <contactcallimarie@gmail.com>
2022-05-07 23:44:48 +0000

    add debug prints + check route for uptime_kuma

--- server.py
+++ server.py
@@ -7,7 +7,10 @@ import random
 import os
 import quart.flask_patch    
 
+
 app = Quart(__name__)
+
+
 
 @app.route("/")
 async def home():
@@ -29,16 +32,25 @@ async def search(filename):
     
 
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/hentai"):
+        print(filename)
+        print(str(f.parent))
         return jsonify(url="https://thino.pics/api/v1/hentai", image=f"https://i.thino.pics/{filename}", dir=f"{str(f.resolve)}")
 
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/helltakerpics"):
+        print(filename)
+        print(str(f.parent))
         return jsonify(url="https://thino.pics/api/v1/helltaker", image=f"https://i.thino.pics/{filename}",dir=f"{str(f.resolve)}")
 
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/neko"):
+        print(filename)
+        print(str(f.parent))
         return jsonify(url="https://thino.pics/api/v1/neko", image=f"https://i.thino.pics/{filename}", dir=f"{str(f.resolve)}")
 
     if f.parent == pathlib.Path("/root/yanpdb/nsfw_cdn/images/tomboy"):
+        print(filename)
+        print(str(f.parent))
         return jsonify(url="https://thino.pics/api/v1/tomboy", image=f"https://i.thino.pics/{filename}", dir=f"{str(f.resolve)}")
+
 
 
 
@@ -47,12 +59,16 @@ async def sendfile(filename=None):
     dir = "/root/yanpdb/nsfw_cdn/"
     p = pathlib.Path(dir)
     for f in p.rglob(filename):
-        print(str(f.parent))
+        pass
     
-    print(f"https://i.thino.pics/{filename}")
+    print(filename)
+    print(str(f.parent))
 
     return await send_from_directory(str(f.parent),filename)    
 
+@app.route('/check', methods=['POST'])
+async def uptime_check():
+    return "Checked!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=2031)
